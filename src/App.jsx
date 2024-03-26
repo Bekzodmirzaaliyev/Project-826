1f672d9ac84a9dd071aab1c45e80c742e25c40fb
Bekzodmirzaaliyev <bekzodmirzaaliyev27@gmail.com>
2024-03-26 18:13:40 +0500

    ASD

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Calculator from './Pages/Calcultor';
-import About from './Pages/About';
-import Contact from './Pages/Contact';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Calculator from './Pages/Calculator';
+import Calendar from './Pages/Calendar';
+import Contacts from './Pages/Contacts';
+import Currency from './Pages/Currency';
+import Gallery from './Pages/Gallery';
+import Swiper from './Pages/Swiper';
+import ToDoList from './Pages/ToDoList';
+import Weather from './Pages/Weather';
+
 
 function App() {
   return (
     <Router>
-      <Switch>
+      <Switch >
         <Route path="/calculator" exact component={Calculator} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
+        <Route path="/cqalendar" component={Calendar} />
+        <Route path="/contacts" component={Contacts} />
+        <Route path="/currency" component={Currency} />
+        <Route path="/galleru" component={Gallery} />
+        <Route path="/swiper" component={Swiper} />
+        <Route path="/todolist" component={ToDoList} />
+        <Route path="/weather" component={Weather} />
+
       </Switch>
     </Router>
   );
