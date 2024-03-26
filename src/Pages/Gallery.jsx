2a6d98873449dddddf90808e9f5b53d6689ac0a8
shahzoda0707 <kamilamirhanova07@gmail.com>
2024-03-26 16:19:03 +0300

    ASD

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Gallery = () => {
   return (
-    <div>Gallery</div>
+    <div>
+      <h1>
+        Красивый летный пейзаж
+      </h1>
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtobX30wIk15uJg5MOl0EqLHRWDdsigjKFidjpcO5PcQ&s'></img>
+      <h1>Морской пейзаж</h1>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtobX30wIk15uJg5MOl0EqLHRWDdsigjKFidjpcO5PcQ&s"></img>
+      <h1></h1>
+      Gallery
+    </div>
   )
 }
 
